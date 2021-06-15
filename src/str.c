0c8c45304a0e9dad9181eca9988b083810c95622
Jose S. Daniel <djose1164@gmail.com>
2021-06-15 17:20:52 -0400

    str's definition init.

---
+++ src/str.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "../include/str.h"
+#include "../include/memory.h"
+
+const char *Str_fmt(const char *_s, ...)
+{
+    va_list args;
+    va_start(args, _s);
+
+    char *buf = NULL;
+    int n = vsnprintf(NULL, 0, _s, args);
+
+    va_end(args);
+
+    if (n >= 0)
+    {
+        va_start(args, _s);
+        buf = memory_allocate((void *)buf, n + 1);
+        if (buf)
+            vsnprintf(buf, n + 1, _s, args);
+        va_end(args);
+    }
+    
+    return buf;
+}
