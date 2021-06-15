527d309f73d0a216d06b56f254d35d8b7257d95a
Jose S. Daniel <djose1164@gmail.com>
2021-06-15 14:01:20 -0400

    Testing.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,19 @@
-int add(int a, int b)
+#include <stdio.h>
+#include <assert.h>
+#include "../include/player.h"
+#include "../include/base.h"
+#include "../include/memory.h"
+
+int main(int argc, char const *argv[])
 {
-    return a + b;
+    struct Player *player = player_init(player);
+    player->base = base_init(player->base);
+
+    base_set_hp(player->base, 100);
+    //printf("hp: %u\n", player->base->hp);
+
+    memory_release(player->base);
+    memory_release(player);
+    memory_check_counter();
+    return 0;
 }
