e4154af28b1a517988a3ed5259d5975c1ef0bff0
Jose S. Daniel <djose1164@gmail.com>
2021-06-15 14:01:48 -0400

    Player definition init.

---
+++ src/player.c
@@ -0,0 +1,27 @@
+/**
+ * @file player.c
+ * @author your name (you@domain.com)
+ * @brief Defefinition of player.h. All methods that modify or work with player
+ * must be here.
+ * @version 0.1
+ * @date 2021-06-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdlib.h>
+
+#include "../include/player.h"
+#include "../include/memory.h"
+
+extern inline struct Player *player_init(struct Player *self)
+{
+    self = memory_allocate(self, sizeof(struct Player));
+    return self;
+}
+
+void player_del(struct Player *const self)
+{
+    if (self)
+        free(self);
+}
