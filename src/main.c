0f48a79c1d2870e5d0b980a8266f10e4345b38fc
Jose S. Daniel Victoriano B <djose1164@gmail.com>
2021-07-12 14:46:45 -0400

    update test driver.

--- src/main.c
+++ src/main.c
@@ -3,17 +3,10 @@
 #include "../include/player.h"
 #include "../include/base.h"
 #include "../include/memory.h"
+#include "../include/game.h"
 
 int main(int argc, char const *argv[])
 {
-    struct Player *player = Player_init(player);
-    player->base = base_init(player->base);
-
-    base_set_hp(player->base, 100);
-    printf("hp: %u\n", player->base->hp);
-
-    memory_release(player->base);
-    memory_release(player);
-    memory_check_counter();
+    game_start();
     return 0;
 }
