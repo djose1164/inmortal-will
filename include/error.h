97189fcec4413b14e76af2010213bc4d1ed0641b
Jose S. Daniel Victoriano B <djose1164@gmail.com>
2021-07-21 20:40:10 -0400

    Added more comments

--- include/error.h
+++ include/error.h
@@ -1,3 +1,19 @@
+/*
+*----------------------------------------------------------------------------*
+|               error.h     -   Public Interface                             |
+*----------------------------------------------------------------------------*
+|   Public interface for error handling. Here's only functions declariations.|
+|   For variables declaritions see error_vars.h.                             |
+*----------------------------------------------------------------------------*
+*/
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#include "error_vars.h"
+
 //----------------------------------------------------------------------------
-// Public interface.
+//                          Public methods.
 //----------------------------------------------------------------------------
+
+#endif //ERROR_H
