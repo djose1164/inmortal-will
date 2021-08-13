49ff2c361be3c120bfd3bd4243c03247a68026ea
Jose S. Daniel <djose1164@gmail.com>
2021-08-12 23:01:24 -0400

    Button module functional (minimal)

---
+++ include/gui/button.h
@@ -0,0 +1,54 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <raylib.h>
+#include "memory.h"
+#include "utils/terminal_colors.h"
+#include "utils/vars.h"
+
+enum ButtonStatus
+{
+    NORMAL,
+    MOUSE_HOVER,
+    PRESSED
+};
+
+struct Button
+{
+    Sound sound;        /* The sound to play. */
+    Texture2D texture;  /* The image to set. */
+    Rectangle source;   /* The container for the texture. */
+    Vector2 pos;        /* The position fo the container. */
+    Color color;        /* The button's color. */
+};
+typedef struct Button Button;
+
+void button_draw(const Button *button);
+
+/**
+ * @brief Initialize a button. Do this before drawing it!
+ * 
+ * @param sound For no sound, pass NULL. Otherwise path to sound.
+ * @param texture Path to texture.
+ * @param rec Pointer to the rectangle area.
+ */
+void button_init(Button *button, const char *str_sound, const char *texture);
+
+/*****************************************************************************/
+/*                                  Private functions:                       */
+/*****************************************************************************/
+
+/**
+ * @brief Allocate memory for a new button.
+ * 
+ * @param ptr The pointer asking for memory.
+ */
+static Button *button_new(Button *ptr);
+
+/**
+ * @brief Update the button.
+ * 
+ * @param ptr Pointer to the button.
+ */
+static void button_update(Button *ptr);
+#endif //BUTTON_H
---
+++ src/gui/button.c
@@ -0,0 +1,76 @@
+#include "gui/button.h"
+#include "utils/vars.h"
+
+static const unsigned NUM_FRAME = 3U;
+
+void button_draw(const Button *button)
+{
+    DrawTextureRec(button->texture, button->source, button->pos, button->color);
+    button_update(button);
+}
+
+void button_init(Button *button, const char *str_sound, const char *texture)
+{
+    // If the sound's path was passed, load it.
+    if (str_sound)
+    {
+        InitAudioDevice();
+        button->sound = LoadSound(str_sound);
+    }
+
+    // Load the texture's source.
+    button->texture = LoadTexture(texture);
+    unsigned frame_height = button->texture.height/NUM_FRAME;
+    // Frame for drawing.
+    button->source.x = 0;
+    button->source.y = frame_height;
+    button->source.width = (float)button->texture.width; 
+    button->source.height = frame_height;
+
+    button->pos.x = screenWidth/2;
+    button->pos.y = screenHeight/3;
+    button->color = WHITE;
+
+    printf("File: %s Function: %s\n", __FILE__, __FUNCTION__);
+}
+
+/*****************************************************************************/
+/*                                  Private functions:                       */
+/*****************************************************************************/
+
+static Button *button_new(Button *ptr)
+{
+    ptr = memory_allocate(ptr, sizeof(Button));
+    return ptr;
+}
+
+static void button_update(Button *button)
+{
+    // Choose what button draw.
+    enum ButtonStatus btn_status = NORMAL;
+    bool play = false;
+    Rectangle btn_bound = {
+        button->pos.x,
+        button->pos.y,
+        button->source.width,
+        button->source.height
+    };
+    
+    if (CheckCollisionPointRec(GetMousePosition(), btn_bound))
+    {
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+            btn_status = PRESSED;
+        else
+            btn_status = MOUSE_HOVER;
+        
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+            play = true;
+    }
+    else
+        btn_status = NORMAL;
+    
+    button->source.y = btn_status * button->source.height;
+
+    if (play)
+        PlaySound(button->sound);
+}
