da5c1f75fe422aa0d7d3eefc2a1f326c5cc09b6b
Jose S. Daniel <djose1164@gmail.com>
2021-08-14 17:39:07 -0400

    list types in its own module.

---
+++ include/utils/list.h
@@ -0,0 +1,20 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdbool.h>
+
+struct List
+{
+    void *item;
+    struct List *next;
+    bool delete;
+};
+typedef struct List List;
+
+struct LinkedList
+{
+    List *head;
+};
+typedef struct LinkedList LinkedList;
+
+#endif //LIST_H
