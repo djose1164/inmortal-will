f1a5c9de6d911b271585742b93dad2f1de74e51c
Jose S. Daniel <djose1164@gmail.com>
2021-08-16 16:14:15 -0400

    Changes app's name.

--- src/main.c
+++ src/main.c
@@ -6,37 +6,17 @@
 #include "str.h"
 #include "../include/memory.h"
 #include "gui/button.h"
-#define APP_NAME "Immortal Will"
 
-const unsigned screenWidth = 1280;
-const unsigned screenHeight = 700;
+#define APP_NAME "InmortalWill"
 
 int main(int argc, char const *argv[])
 {
-    // Window size.
-    
-    
-    InitWindow(screenWidth, screenHeight, APP_NAME);
-    const char *msg = "aloha";
-   /* char temp[MAX_INPUT+1] = "\0";
-    const unsigned input_fontSize = 32;
-    TextInput text_input;
-    Rectangle rec = {
-        100, 
-        100, 
-        ((int)input_fontSize/2)*MAX_INPUT + (int)100/4, 
-        input_fontSize+10
-    };
-    textInput_init(&text_input, &rec, MAX_INPUT, input_fontSize);
-    text_input.msg = temp; */
+    // Windows size.
+    const unsigned screenWidth = 1280;
+    const unsigned screenHeight = 700;
 
-    Button button;
-     printf("File: %s Function: %s Line: %d\n", __FILE__, __FUNCTION__, __LINE__);
-    button_init(&button, "resources/buttonfx.wav", "resources/button.png");
-     printf("File: %s Function: %s Line: %d\n", __FILE__, __FUNCTION__, __LINE__);
+    InitWindow(screenWidth, screenHeight, APP_NAME);
     
-    
-    ClearBackground(BLACK);
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -44,12 +24,11 @@ int main(int argc, char const *argv[])
         ClearBackground(BLACK);
         // Draw the app's title.
         DrawText(APP_NAME, (int)screenWidth/2.5, 1, 64, RED);
-        //textInput_draw(&text_input);
-        button_draw(&button);
-        //game_start();
+
+        // TODO: Stuff.
+        
     EndDrawing();    
     }
-    //memory_release((void *)msg);
-    //atexit(memory_check_counter);
+
     return 0;
 }
