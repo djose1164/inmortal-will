13f9ddec66d80f09b8568c8cd165b2eddc01a1f5
Jose S. Daniel <djose1164@gmail.com>
2021-09-01 23:09:40 -0400

    Add needed include.

--- include/main.h
+++ include/main.h
@@ -1,11 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "core/physics.h"
+#include "game.h"
 #include "characters/player.h"
+#include "gui/button.h"
+#include "gui/text_input.h"
+#include "gui/welcome.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <raylib.h>
 
+#define APP_NAME "InmortalWill"
 
 #endif //MAIN_H
