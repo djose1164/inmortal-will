35af14c3a9e307e0798a70be098668f2ecb10e6f
Jose S. Daniel <djose1164@gmail.com>
2021-09-01 23:15:09 -0400

    update test driver.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-#define APP_NAME "InmortalWill"
-
 int main(int argc, char const *argv[])
 {
     // Windows size.
@@ -10,37 +8,11 @@ int main(int argc, char const *argv[])
 
     // The first thing. Init the window.
     InitWindow(screenWidth, screenHeight, APP_NAME);
-
-    Player pj = {
-        .pos.x = 0,
-        .pos.y = GetScreenHeight() / 2,
-        .skin = LoadTexture("resources/player.png"),
-        .being.weight = .5,
-        .being.str = .5
-    };
-
-    SetTargetFPS(60);
-    while (!WindowShouldClose())
-    {
-        Player_goto(&pj);
-
-        BeginDrawing();
-            ClearBackground(BLACK);
-
-            // Draw the app's title.
-            DrawText(APP_NAME, (int)screenWidth / 2.5, 0, 64, RED);
-
-            // Stats.
-            DrawText(TextFormat("position: (%.3f, %.3f)", pj.pos.x, pj.pos.y), 0, 
-                     0, 32, RED);
-            DrawText(TextFormat("time: %.3fs", 5.5), 0, 32, 32, RED);
-            DrawText(TextFormat("vel: %.3fm/s", physics_get_velocity(&pj.being, 2.5)), 0,
-                    64, 32, RED);
-
-            Player_draw(&pj);
-            //game_start();
-        EndDrawing();
-    }
+    SetTargetFPS(30);
+    
+    game_start();
+    
+    
     CloseWindow();
     return 0;
 }
