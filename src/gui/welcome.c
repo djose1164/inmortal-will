10500ccc2d5f0cc26417a150100020247349e823
Jose S. Daniel <djose1164@gmail.com>
2021-09-01 23:10:25 -0400

    Add more definition.

--- src/gui/welcome.c
+++ src/gui/welcome.c
@@ -1,14 +1,44 @@
 #include "gui/welcome.h"
-#include <raylib.h>
 
 #define MAX_INPUT_CHARS 20
 
-void welcome_start(void)
+bool done = false;
+
+static __attribute__((always_inline)) void set_end(void) 
 {
-    welcome_message("Welcome to Immortal Will!");
+    done = true;
+}
+
+bool welcome_start(Player *const player)
+{
+    TextInput text_input;
+    Rectangle rec = {GetScreenWidth()/3.5, 125, 500, 100};
+    char *line = MemAlloc(sizeof *line * 1024);
+    textInput_init(&text_input, &rec, 20, 46, line, set_end);
+
+    Button button;
+    button_init(&button, NULL, "resources/start_button_2.png", set_end);
+
+    while (!WindowShouldClose())
+    {
+        if (done)
+        {
+            Player_set_name(player, text_input.msg);
+            return true;
+        }
+        
+        BeginDrawing();
+            ClearBackground(BLACK);
+            welcome_message("Welcome to Immortal Will!");
+
+            textInput_draw(&text_input);
+            button_draw(&button);
+        EndDrawing();
+    }
+    return false;
 }
 
 static void welcome_message(const char *msg)
 {
-    DrawText(msg, 100, 100, 30, RED);
+    DrawText(msg, GetScreenWidth()/4, 0, 50, RED);
 }
