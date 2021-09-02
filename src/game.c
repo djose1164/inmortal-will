2da4dcb48cf2a039f26d1cd2c29ba23ed196c270
Jose S. Daniel <djose1164@gmail.com>
2021-09-01 23:14:43 -0400

    init.

---
+++ src/game.c
@@ -0,0 +1,20 @@
+#include "game.h"
+
+void game_start()
+{
+    Player player;
+    memset(&player, 0, sizeof player);
+
+    if (!welcome_start(&player))
+        return;
+    
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText(TextFormat("Welcome %s.", player.being.attrs.name), 0, 200, 
+                     36, RED);
+        EndDrawing();
+    }
+}
+
