29fe61ea9753dc396ca7d3785494298a7c4e94b4
Jose S. Daniel <djose1164@gmail.com>
2021-09-01 23:09:19 -0400

    update: func declaration and includes.

--- include/game.h
+++ include/game.h
@@ -4,12 +4,12 @@
 #include "characters/player.h"
 #include "memory.h"
 #include "gui/text_input.h"
-#include "vars.h"
 #include <string.h>
 #include <raylib.h>
+
 void game_start();
 
 static void set_profession(Player *const self);
-static void game_set_player_name(Player *self);
+void game_setup_player(Player *const self, const char *name);
 
 #endif //GAME_H
