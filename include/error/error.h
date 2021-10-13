d442dcb634972cf1bfd8be7bdd945ad67aa7a5f4
Jose S. Daniel <djose1164@gmail.com>
2021-10-13 09:28:52 -0400

    Added more functions.

--- include/error/error.h
+++ include/error/error.h
@@ -11,9 +11,38 @@
 #define ERROR_H
 
 #include "error_vars.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 //----------------------------------------------------------------------------
 //                          Public methods.
 //----------------------------------------------------------------------------
+
+/**
+ * @brief Get the meaning of the code.
+ * 
+ * @param code Error code.
+ * @return char* The code's meaning.
+ */
+char * err_code_string(enum ErrCode code);
+
+/*      Private.        */
+
+/**
+ * @brief Check if the given code is valid.
+ * 
+ * @param code The error code.
+ * @return true The code is valid.
+ * @return false The code is invalid.
+ */
+static bool err_code_is_valid(unsigned code);
+
+/**
+ * @brief Show a message and die.
+ * 
+ * @param errmsg Message describing the reason.
+ */
+static void err_die(char *errmsg);
 
 #endif //ERROR_H
