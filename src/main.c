d4f38b907c55b08196554c334c5ac4b091df388b
Jose S. Daniel <djose1164@gmail.com>
2022-01-09 18:37:07 -0400

    Change resoultion and fps.

--- src/main.c
+++ src/main.c
@@ -3,21 +3,15 @@
 int main(int argc, char const *argv[])
 {
     // Windows size.
-    const unsigned screenWidth = 1280;
-    const unsigned screenHeight = 700;
+    const unsigned screenWidth = 1080;
+    const unsigned screenHeight = 600;
 
     // The first thing. Init the window.
     InitWindow(screenWidth, screenHeight, APP_NAME);
-    SetTargetFPS(30);
+    SetTargetFPS(60);
     
     game_start();
     
-    while(!WindowShouldClose())
-    {
-        BeginDrawing();
-            
-        EndDrawing;
-    }
     
     CloseWindow();
     return 0;
