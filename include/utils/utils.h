01e90eded18bfb6dfbdaa864e6e05de8c5c321a4
Jose S. Daniel <djose1164@gmail.com>
2022-01-09 18:37:36 -0400

    Add file checking.

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -4,6 +4,7 @@
 #include "utils/list.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * @brief Will run through the linked list until find a NULL pointer. 
@@ -16,8 +17,8 @@
  * 
  * TODO: Add more abstraction/polymorphism.
  */
-#define for_each_item(item, l)    \
-    for (List *(item) = (l)->head; (item); (item) = (item)->next)
+#define for_each_item(item, l) \
+    for (List * (item) = (l)->head; (item); (item) = (item)->next)
 
 /**
  * @brief Go through all item of a given array.
@@ -25,18 +26,32 @@
  * @param item Var that will be use for each iteration.
  * @param array Array to go through.
  */
-#define for_each(item, array)                               \
-    for (int keep = 1, count = 0,                       \
-         size = sizeof((array)) / sizeof(*(array));         \
-         keep && count != size;                             \
-         keep = !keep, ++count)                             \
-        for (item = (array) + count; keep; keep = !keep)    
+#define for_each(item, array)                          \
+    for (int keep = 1, count = 0,                      \
+        size = sizeof((array)) / sizeof(*(array));     \
+        keep && count != size;                         \
+        keep = !keep, ++count)                         \
+        for (item = (array) + count; keep; keep = !keep)
 
-
-static inline void utils_clear_terminal(void)   
+static inline void utils_clear_terminal(void)
 {
     fflush(stdout);
     system("clear||cls");
+}
+
+/**
+ * @brief Check weather the file exists or not.
+ * 
+ * @param fname Path to file.
+ * @return true The file exists.
+ * @return false The file does not exist.
+ */
+static inline bool utils_file_exists(const char *fname)
+{
+    if (access(fname, F_OK))
+        return true;
+    else
+        return false;
 }
 
 #endif //UTILS_H
