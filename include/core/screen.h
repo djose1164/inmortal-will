ae053abf8199a170c2ed5043a68169cc7a3dfd1a
Jose S. Daniel <djose1164@gmail.com>
2022-01-14 15:52:29 -0400

    In standby.

---
+++ include/core/screen.h
@@ -0,0 +1,11 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+typedef struct Screen Screen;
+struct Screen
+{
+    
+};
+
+
+#endif //SCREEN_H
---
+++ include/core/screen_manager.h
@@ -0,0 +1,6 @@
+#ifndef SCREEN_MANAGER_H
+#define SCREEN_MANAGER_H
+
+#include "core/object.h"
+
+#endif //SCREEN_MANAGER_H
