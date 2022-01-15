acb84287286759693d4e61f7c3afc795633b9398
Jose S. Daniel <djose1164@gmail.com>
2022-01-15 00:40:39 -0400

    screen_manager init decleration.

--- include/core/screen_manager.h
+++ include/core/screen_manager.h
@@ -1,6 +1,21 @@
 #ifndef SCREEN_MANAGER_H
 #define SCREEN_MANAGER_H
 
-#include "core/object.h"
+#include "core/screen.h"
+
+/**
+ * @brief ScreenManager is responsable to render its current screen. To render
+ * any other screen the current to point to that screen.
+ * 
+ */
+struct ScreenManager
+{
+    Screen *current;
+
+    void (*render)(const Screen *screen);
+};
+typedef struct ScreenManager ScreenManager;
+
+static void screenManager_render(const ScreenManager *manager);
 
 #endif //SCREEN_MANAGER_H
