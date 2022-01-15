fa799e1675ead8a1924705cd0977737eff210efc
Jose S. Daniel <djose1164@gmail.com>
2022-01-15 16:08:51 -0400

    Innecesary.

--- src/core/screen_manager.c
+++
@@ -1,9 +0,0 @@
-#include "core/screen_manager.h"
-
-static void screenManager_render(const ScreenManager *manager)
-{
-    BeginDrawing();
-    manager->current->update(manager->current);
-    manager->current->render(manager->current);
-    EndDrawing();
-}
