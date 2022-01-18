3cef461d7d4e3d9178a15d344395ef84b8b43f88
Jose S. Daniel <djose1164@gmail.com>
2022-01-17 22:36:37 -0400

    Add object allocation to its super member.

--- src/graphics/texture.c
+++ src/graphics/texture.c
@@ -5,12 +5,16 @@ IW_Texture *texture_init(const char *rpath)
     puts("Creating texture...");
     if (!utils_file_exists(rpath))
         memory_die("texture_init(): file does not exist.");
+        
+    Object *obj = object_init("Texture", TEXTURE);
     IW_Texture *self = memory_allocate(self, sizeof *self);
-    self->obj_super = NULL;
+    self->obj_super = obj;
     self->rpath = rpath;
+
     self->_texture2D = LoadTexture(rpath);
     self->get_height = texture_get_height;
     self->get_width = texture_get_width;
+    self->del = texture_del;
     puts("Creating texture... Done!");
 
     return self;
@@ -24,4 +28,13 @@ static unsigned texture_get_width(const IW_Texture *self)
 static unsigned texture_get_height(const IW_Texture *self)
 {
     return self->_texture2D.height;
+}
+
+static void texture_del(IW_Texture *self)
+{
+    puts("Deleting texture...");
+    self->obj_super->del(self->obj_super);
+    UnloadTexture(self->_texture2D);
+    memory_release(self);
+    puts("Deleting texture... Done!");
 }
