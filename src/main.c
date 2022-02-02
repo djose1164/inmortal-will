2cb133843e6fa8cdbba7d127fad506eec80a9e35
Jose S. Daniel <djose1164@gmail.com>
2022-02-01 23:06:57 -0400

    Update driver.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,15 @@
 
 void draw_background(const Frame *self)
 {
-    DrawTextureEx(*self->get_texture(self), self->position, 0.f, 2.f, WHITE);
+    static float scrolling = 0.f;
+    scrolling -= 1.f;
+    const float max = -self->_texture->_texture2D.width * 4.5f;
+    if (scrolling <= max)
+        scrolling = 0;
+    DrawTextureEx(*self->get_texture(self), (Vector2){scrolling, self->position.y},
+                  0.f, 4.5f, WHITE);
+    DrawTextureEx(*self->get_texture(self), (Vector2){self->get_texture_width(self) * 4.5f 
+                  + scrolling, self->position.y}, 0.f, 4.5f, WHITE);
 }
 
 int main(int argc, char const *argv[])
@@ -15,36 +23,36 @@ int main(int argc, char const *argv[])
     InitWindow(screenWidth, screenHeight, APP_NAME);
     SetTargetFPS(60);
 
-    IW_Texture *texture = texture_init("resources/player.png");
-    Frame *frame = frame_init(texture, &(Vector2){100, (float)screenWidth/3.5f}, &WHITE);
-    Player *player = player_init(living_init("djose1164", PLAYER, frame));
+    IW_Texture *texture = texture_init("resources/spaceship-draft.png");
+    Frame *frame = frame_init(texture, &(Vector2){100, (float)screenWidth / 3.5f}, &WHITE);
+    IW_Texture *weapon = texture_init("resources/laser.png");
+    Player *player = player_init(living_init("djose1164", PLAYER, frame), weapon);
     struct Update update = {
         .num = 1,
         .objcs = &player,
-        .update_arr = &player->update
-    };
+        .update_arr = &player->update};
     struct Cleanup cleanup = {
         .num = 1,
         .objcs = &player,
         .del_arr = &player->del,
     };
 
-    Frame * background = frame_init(texture_init("./resources/foreground.png"), &(Vector2){0, 0}, &WHITE);
+    Frame *background = frame_init(texture_init("./resources/space_with_stars.png"), &(Vector2){0, 0}, &WHITE);
     background->draw = draw_background;
-    Screen *current = screen_init(string_init("Testing"), frame, background, NULL, &update, &cleanup);    
+    Screen *current = screen_init(string_init("Testing"), player, NULL, NULL, &update, &cleanup);
 
-    
     while (!WindowShouldClose())
     {
         current->update(current);
         BeginDrawing();
-            ClearBackground(LIGHTGRAY);
-            current->render(current);
+        ClearBackground(BLACK);
+        current->render(current);
         EndDrawing();
     }
-    
+
     current->del(current);
     CloseWindow();
+    weapon->del(weapon);
     memory_check_counter();
     return 0;
 }
