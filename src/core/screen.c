a25378f4429133efd5ae53ba329f5553a76910c2
Jose S. Daniel <djose1164@gmail.com>
2022-02-03 15:36:15 -0400

    Fix and update.

--- src/core/screen.c
+++ src/core/screen.c
@@ -6,24 +6,24 @@ static inline void screen_update_camera2D(Screen *const self)
     self->camera.target = (Vector2){_target->position.x, 0};
 }
 
-Screen *screen_init(String *title, void *target, Frame *background, const void *frames,
-                    const struct Update *_update, const struct Cleanup *_cleanup)
+Screen *screen_init(String *title, void *target, Frame *background, const void *frames, size_t nframes,
+                    const struct ScreenManager *manager)
 {
     puts("Creating screen...");
     Frame *_target = ((Player *)target)->base_super->frame;
     Screen *self = memory_allocate(sizeof *self);
     self->object_super = object_init("Screen", SCREEN);
     self->title = string_init(title);
+    
     self->_target = target;
     self->frames = frames;
+    self->frame_len = nframes;
 
     screen_init_camera2D(self);
 
     self->background = background;
-    assert(_update && _cleanup);
-    self->update_struct = _update;
-    self->cleanup_struct = _cleanup;
-
+    assert(manager);
+    self->manager = manager;
     self->update = screen_update;
     self->render = screen_render;
     self->del = screen_del;
@@ -52,42 +52,41 @@ static void screen_init_camera2D(Screen *const self)
 
 static void screen_render(const Screen *self)
 {
-    puts("Screen rendering...");
-    Player **frames = (Player *)self->frames;
+    void **frames = (void **)self->frames;
     BeginMode2D(self->camera);
     if (self->background)
         self->background->draw(self->background);   
     DrawText("Holala", 50, 50, 36, RED);
     assert(frames);
-   /* for (size_t i = 0; i < 2; i++)
-        frames[i].draw(&frames[i]);*/
-    (*frames)->draw((*frames));
-    frames[1]->draw(frames[1]);
-    puts("Here!");
+    printf("frames_len: %zu\n", self->frame_len);
+    for (size_t i = 0; i < self->frame_len; i++)
+        self->manager->draw[i](frames[i]);
     EndMode2D();
-    puts("Screen rendering... Done!");
 }
 
 static void screen_update(const Screen *self)
 {
-    size_t num = self->update_struct->num;
+    void **targets = (void **)self->frames;
+    size_t num = self->frame_len;
     for (size_t i = 0; i < num; i++)
-        self->update_struct->update_arr[i](self->update_struct->objcs[i]);
+        self->manager->update[i](targets[i]);
+    //self->manager->update[1](targets[1])
     screen_update_camera2D(self);
 }
 
 static void screen_cleanup(Screen *self)
 {
+    void **targets = (void **)self->frames;
     puts("Screen cleaning up...");
-    for (size_t i = 0; i < self->cleanup_struct->num; i++)
-        self->cleanup_struct->del_arr[i](self->cleanup_struct->objcs[i]);
+    for (size_t i = 0; i < self->frame_len; i++)
+        self->manager->del[i](targets[i]);
     puts("Screen cleaning up... Done");
 }
 
 static void screen_del(Screen *self)
 {
     puts("Deleting screen...");
-    if (self->cleanup_struct)
+    if (self->manager)
         self->cleanup(self);
 
     if (self->background)
