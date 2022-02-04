fb5955f1e76ac3714b26b83013d0a0665f69596a
Jose S. Daniel <djose1164@gmail.com>
2022-02-04 19:38:43 -0400

    Add guards and optimization.

--- src/core/screen.c
+++ src/core/screen.c
@@ -3,7 +3,7 @@
 
 static inline void screen_update_camera2D(Screen *const self)
 {
-    Frame *_target = ((Player *)self->_target)->base_super->frame;   
+    Frame *_target = ((Player *)self->_target)->base_super->frame;
     self->camera.target = (Vector2){_target->pos.x, 0};
 }
 
@@ -11,18 +11,18 @@ Screen *screen_init(String *title, void *target, Frame *background, const void *
                     const struct ScreenManager *manager)
 {
     puts("Creating screen...");
-    Frame *_target = ((Player *)target)->base_super->frame;
     Screen *self = memory_allocate(sizeof *self);
-    self->title = string_init(title);
-    
+    Frame *_target = NULL;
+    if (target)
+        _target = ((Player *)target)->base_super->frame;
+
     self->_target = target;
+    screen_init_camera2D(self);
+    self->title = title;
     self->frames = frames;
     self->frame_len = nframes;
 
-    screen_init_camera2D(self);
-
     self->background = background;
-    assert(manager);
     self->manager = manager;
     self->update = screen_update;
     self->render = screen_render;
@@ -36,14 +36,21 @@ Screen *screen_init(String *title, void *target, Frame *background, const void *
 static void screen_init_camera2D(Screen *const self)
 {
     puts("Init camera...");
-    assert(self->_target);
-    assert(((Player *)self->_target)->base_super->frame);
-    Player *temp = (Player *)self->_target;
-    Frame *_target = temp->base_super->frame;
-    assert(_target);
-    float x_offset = _target->get_texture_width(_target) / 2.f;
-    //float y_offset = self->_target->get_texture_height(self->_target) / 2.f;
-    self->camera.target = _target->pos;
+    Vector2 pos;
+    float x_offset;
+    if (self->_target)
+    {
+        Player *temp = (Player *)self->_target;
+        Frame *_target = temp->base_super->frame;
+        x_offset = _target->get_texture_width(_target) / 2.f;
+        pos = _target->pos;
+    }
+    else
+    {
+        pos = (Vector2){GetScreenWidth() / 2, GetScreenHeight() / 2};
+        x_offset = GetScreenWidth() / 2.f;
+    }
+    self->camera.target = pos;
     self->camera.offset = (Vector2){x_offset, 0};
     self->camera.rotation = 0.f;
     self->camera.zoom = 1.f;
@@ -55,7 +62,7 @@ static void screen_render(const Screen *self)
     void **frames = (void **)self->frames;
     BeginMode2D(self->camera);
     if (self->background)
-        self->background->draw(self->background);   
+        self->background->draw(self->background);
     DrawText("Holala", 50, 50, 36, RED);
     assert(frames);
     for (size_t i = 0; i < self->frame_len; i++)
@@ -66,13 +73,15 @@ static void screen_render(const Screen *self)
 
 static void screen_update(const Screen *self)
 {
-    void **targets = (void **)self->frames;
-    size_t num = self->frame_len;
-    for (size_t i = 0; i < num; i++)
-        if (targets[i])
-        self->manager->update[i](targets[i]);
-    //self->manager->update[1](targets[1])
-    screen_update_camera2D(self);
+    if (self->frames)
+    {
+        void **targets = (void **)self->frames;
+        size_t num = self->frame_len;
+        for (size_t i = 0; i < num; i++)
+            self->manager->update[i](targets[i]);
+        // self->manager->update[1](targets[1])
+        screen_update_camera2D(self);
+    }
 }
 
 static void screen_cleanup(Screen *self)
@@ -89,12 +98,13 @@ static void screen_del(Screen *self)
     puts("Deleting screen...");
     if (self->manager)
         self->cleanup(self);
-
     if (self->background)
         self->background->del(self->background, true);
-
-    self->title->del(self->title);
-
+    // if (self->title && strcmp(self->title, APP) != 0)
+    {
+        self->title->del(self->title);
+        puts("*-*-*-Screen string deleted*-*-*-");
+    }
     memory_release(self);
     puts("Deleting screen... Done!");
 }
