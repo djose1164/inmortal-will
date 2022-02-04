13678a65b70e3cd7a6c6322511994629e9787b06
Jose S. Daniel <djose1164@gmail.com>
2022-02-04 17:30:36 -0400

    Add needed includes and mem debug info.

--- src/item/weapon.c
+++ src/item/weapon.c
@@ -1,5 +1,7 @@
 #include "item/weapon.h"
 #include "characters/alien.h"
+#include <stdio.h>
+#include <assert.h>
 
 struct Laser
 {
@@ -8,6 +10,9 @@ struct Laser
     Frame *frame;
     bool launched;
 };
+
+static size_t created_lasers;
+static size_t deallocated_lasers;
 
 Laser weapon_create_lasers(unsigned quantity)
 {
@@ -62,16 +67,31 @@ Laser weapon_next_laser(Laser laser, Vector2 *pos)
     {
         current->frame = frame_init(current->skin, pos, &WHITE);
         current->launched = true;
+        created_lasers++;
         return current;
     }
     puts("No laser avaible!!!");
     return NULL;
 }
 
-void weapon_laser_destroy(Laser laser)
+static void weapon_laser_destroy(Laser laser, bool del_texture)
 {
     laser->launched = false;
-    laser->frame->del(laser->frame, false);
+    laser->frame->del(laser->frame, del_texture);
+    deallocated_lasers++;
+}
+
+void weapon_destroy_all(Laser laser)
+{
+    laser->skin->del(laser->skin);
+    for (size_t i = 0; i < MAX_NUMS_OF_LASER; i++)
+        if (laser[i].launched)
+            weapon_laser_destroy(&laser[i], false);
+    if (deallocated_lasers == created_lasers)
+        puts("Mem in lasers is Ok!");
+    else
+        printf("Mem in lasers is NOT Ok!\n"
+               "Allocated: %zu \t Deallocated: %zu\n", created_lasers, deallocated_lasers);
 }
 
 void weapon_update_lasers(Laser laser)
@@ -82,17 +102,15 @@ void weapon_update_lasers(Laser laser)
     weapon_check_impact(laser);
     assert(laser);
     volatile double time = 60 * (.035f / laser->speed);
-    printf("laser's speed: %.3f\n", laser->speed);
     for (size_t i = 0; i < MAX_NUMS_OF_LASER; i++)
     {
         if (laser[i].launched)
         {
             laser[i].frame->pos.x += time * laser->speed;
             if (laser[i].frame->pos.x > GetScreenWidth() * 1.5)
-                weapon_laser_destroy(&laser[i]);
+                weapon_laser_destroy(&laser[i], true);
         }
     }
-    printf("laser->pos.y = %.3f\n", laser->frame->pos.y);
 }
 
 void weapon_draw_lasers(Laser laser)
