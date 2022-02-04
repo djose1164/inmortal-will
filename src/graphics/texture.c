baa025ef81ba0ddcdb30e18897fb8c1f9ceadabe
Jose S. Daniel <djose1164@gmail.com>
2022-02-04 17:23:37 -0400

    Update some functions. Add needed funcs.

--- src/graphics/texture.c
+++ src/graphics/texture.c
@@ -1,22 +1,34 @@
 #include "graphics/texture.h"
+#include "utils/utils.h"
+#include "core/error.h"
+#include "core/memory_p.h"
+#include <stdio.h>
+
+static size_t allocated, deallocated;
+
+void texture_memstats(void)
+{
+    if (allocated == deallocated)
+        puts("Everything on textures is Ok.");
+    else
+        printf("%s:\n Allocated: %zu \t Deallocated: %zu\n", allocated, deallocated);
+}
 
 IW_Texture *texture_init(const char *rpath)
 {
     puts("Creating texture...");
     if (!utils_file_exists(rpath))
-        memory_die("texture_init(): file does not exist.");
+        ERROR_DIE("file does not exist.");
         
-    Object *obj = object_init("Texture", TEXTURE);
     IW_Texture *self = memory_allocate(sizeof *self);
-    self->obj_super = obj;
+    assert(self->rpath);
     self->rpath = rpath;
-
     self->_texture2D = LoadTexture(rpath);
     self->get_height = texture_get_height;
     self->get_width = texture_get_width;
     self->del = texture_del;
     puts("Creating texture... Done!");
-
+    allocated++;
     return self;
 }
 
@@ -33,8 +45,8 @@ static unsigned texture_get_height(const IW_Texture *self)
 static void texture_del(IW_Texture *self)
 {
     puts("Deleting texture...");
-    self->obj_super->del(self->obj_super);
     UnloadTexture(self->_texture2D);
     memory_release(self);
+    deallocated++;
     puts("Deleting texture... Done!");
 }
