76d4326f2c31c6d9a0eec9a765c6b98b38505966
Daniel Victoriano <djose1164@gmail.com>
2022-02-14 15:10:01 +0000

    Rename to laser.

--- include/item/laser.h
+++ include/item/laser.h
@@ -17,35 +17,35 @@ typedef Type Owner;
  * @param quantity Number of lasers to create.
  * @return Laser Pointer to array of lasers.
  */
-T weapon_create_lasers(unsigned quantity, Owner direction);
+T laser_create_lasers(unsigned quantity, Owner direction);
 
-T weapon_next_laser(T laser, Vector2 *pos);
+T laser_next_laser(T laser, Vector2 *pos);
 
-void weapon_update_lasers(T laser);
+void laser_update_lasers(T laser);
 
-void weapon_draw_lasers(T laser);
+void laser_draw_lasers(T laser);
 
-void weapon_set_frame(T laser, Frame *frame);
+void laser_set_frame(T laser, Frame *frame);
 
-void weapon_set_lauched(T laser, bool launched);
+void laser_set_lauched(T laser, bool launched);
 
-void weapon_set_pos(T laser, Vector2 *pos);
+void laser_set_pos(T laser, Vector2 *pos);
 
-void weapon_set_texture(T laser, IW_Texture texture);
+void laser_set_texture(T laser, IW_Texture texture);
 
-void weapon_destroy_all(T laser);
+void laser_destroy_all(T laser);
 
-const IW_Texture *weapon_get_texture(T laser);
+const IW_Texture *laser_get_texture(T laser);
 
-Vector2 weapon_get_pos(T Laser);
+Vector2 laser_get_pos(T Laser);
 
-bool weapon_is_laser_attacking(T laser);
+bool laser_is_laser_attacking(T laser);
 
 /****************************************************************************/
 /*                            Private Functions.                            */
 /****************************************************************************/
 
-static void weapon_laser_destroy(Laser laser);
+static void laser_laser_destroy(Laser laser);
 
 #undef T
 #endif /* WEAPON_H */
